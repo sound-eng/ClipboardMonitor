1dabc97e9a393e82b664431908cae9eb3e2313cb
Oleh Naumenko <2970642+sound-eng@users.noreply.github.com>
2026-07-09 17:09:41 +0300

    add gitignore with common rules for Swift Xcode project

---
+++ ClipboardMonitor/PlatformTypes.swift
@@ -0,0 +1,14 @@
+//
+//  PlatformTypes.swift
+//  ClipboardMonitor
+//
+
+#if os(macOS)
+import AppKit
+typealias PlatformImage = NSImage
+typealias PlatformColor = NSColor
+#else
+import UIKit
+typealias PlatformImage = UIImage
+typealias PlatformColor = UIColor
+#endif
